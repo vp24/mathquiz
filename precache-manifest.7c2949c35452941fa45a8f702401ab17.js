1f9baf98c04fe156696c628b3685b751e05f432f
dishonestea <vishay8@gmail.com>
2020-07-15 17:35:13 -0500

    Updates

---
+++ precache-manifest.7c2949c35452941fa45a8f702401ab17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29f98de6b29903ee4b936d812c38d45",
+    "url": "/mathquiz/index.html"
+  },
+  {
+    "revision": "03202afd7c6b8a2ccbe5",
+    "url": "/mathquiz/static/css/main.ea275c24.chunk.css"
+  },
+  {
+    "revision": "b0c7276852eec0a4ba8d",
+    "url": "/mathquiz/static/js/2.9aa03823.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mathquiz/static/js/2.9aa03823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03202afd7c6b8a2ccbe5",
+    "url": "/mathquiz/static/js/main.74989b93.chunk.js"
+  },
+  {
+    "revision": "84d5cd8882d9e4d5e6be",
+    "url": "/mathquiz/static/js/runtime-main.9333d898.js"
+  }
+]);
